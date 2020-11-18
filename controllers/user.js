c7fbd7801ea7693e37c55a29b0bf2d2f026c0066
julio cesar naranjo quintero <juliocesarn2010@hotmail.com>
2020-11-17 20:47:53 -0500

    se agrega ruta get por id, update y delete de user

--- controllers/user.js
+++ controllers/user.js
@@ -1,3 +1,4 @@
+const { response } = require('express');
 const User = require('../models/user');
 const bcrypt = require('bcryptjs');
 
@@ -15,6 +16,32 @@ const getUsers = async (req, res) => {
         return res.status(400)
         .json({
             msg: 'error al consultar los usuarios.'
+        });  
+    }
+};
+
+const getUser = async (req, res) => {
+
+    const uid = req.params.id;
+    try {
+        // validar si existe user
+        const user = await User.findById(uid);
+
+        if ( !user ) {
+            return res.status(404).json({
+                msg: 'No existe usuario con ese id'
+            });
+        }
+
+        return res.status(200)
+        .json({
+            user
+        });
+    } catch (error) {
+        console.log(error);
+        return res.status(400)
+        .json({
+            msg: 'error al consultar el usuario.'
         });  
     }
 };
@@ -52,7 +79,67 @@ const saveUser = async (req, res) => {
         }
 };
 
+const updateUser = async (req, res) => {
+
+    const uid = req.params.id;
+    try {
+
+        // validar que el usuario existe
+        const usuarioDB = await User.findById( uid );
+
+        if ( !usuarioDB ) {
+            return res.status(404).json({
+                msg: 'No existe un usuario con ese id'
+            });
+        }
+
+        const { password, image, ...campos } = req.body;
+        const userUpdated = await User.findByIdAndUpdate( uid, campos, { new: true} );
+
+        return res.status(200).json({
+            msg: 'usuario actualizado',
+            userUpdated
+        });
+
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            msg: 'Contacta con el administrador'
+        });
+    }
+};
+
+const deleteUser = async (req, res = response ) => {
+
+    const uid = req.params.id;
+    try {
+
+        // validar que el usuario existe
+        const usuarioDB = await User.findById( uid );
+
+        if ( !usuarioDB ) {
+            return res.status(404).json({
+                msg: 'No existe un usuario con ese id'
+            });
+        }
+
+        await User.findByIdAndDelete( uid );
+
+        return res.status(200).json({
+            msg: 'Usuario eliminado'
+        });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            msg: 'Conctata al administrador'
+        });
+    }
+};
+
 module.exports = {
     getUsers,
-    saveUser
-}
+    getUser,
+    saveUser,
+    updateUser,
+    deleteUser
+};
--- routes/user.js
+++ routes/user.js
@@ -2,14 +2,19 @@ const express = require('express');
 const { check } = require('express-validator');
 
 // controllers
-const { getUsers, saveUser } = require('../controllers/user');
+const { getUsers, 
+        saveUser,
+        updateUser, 
+        deleteUser,
+        getUser } = require('../controllers/user');
 const { validarCampos } = require('../midlewares/validar-campos');
 
 const router = express.Router();
 
-/// /api/user
+/// /api/user/
 
 router.get('/', getUsers);
+router.get('/:id', getUser);
 router.post('/',
  [ 
      check('name', 'El campo name es obligatorio').not().isEmpty(),
@@ -18,6 +23,18 @@ router.post('/',
      check('email', 'El campo email es obligatorio').isEmail(),
      validarCampos,
  ], 
- saveUser );
+ saveUser 
+ );
+router.put('/:id', 
+    [
+        check('name', 'El campo name es obligatorio').not().isEmpty(),
+        check('lastName', 'El campo lastName es obligatorio').not().isEmpty(),
+        check('role', 'El campo role es obligatorio').not().isEmpty(),
+        check('email', 'El campo email es obligatorio').isEmail(),
+        validarCampos,
+    ],
+    updateUser
+ );
+router.delete('/:id', deleteUser);
 
 module.exports = router;
